1557107c8aa78fa5c67ae07371ed3f5e3a655dda
Joachim Larsson <joachimlarsson90@gmail.com>
2019-03-26 16:25:59 +0100

    Add trigger button
    * Add mesh and script for a trigger button
    * Triggered by moving the character on top of the button
    * Add button to SampleScene
    * Create prefab of character and change the capsule collider

---
+++ Tords Puzzle Adventure/Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour
+{
+	bool triggerEntered = false;
+	Vector3 defaultScale;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (GetComponent<Collider>().isTrigger)
+		{
+			triggerEntered = true;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (GetComponent<Collider>().isTrigger)
+		{
+			triggerEntered = false;
+		}
+	}
+
+	// Start is called before the first frame update
+	void Awake()
+    {
+		defaultScale = transform.localScale;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (triggerEntered)
+		{
+			float scaleY = transform.localScale.y - 0.1f * Time.deltaTime;
+			scaleY = Mathf.Max(scaleY, defaultScale.y * 0.1f);
+
+			transform.localScale = new Vector3(transform.localScale.x, scaleY, transform.localScale.z);
+		}
+		else
+		{
+			float scaleY = transform.localScale.y + 0.1f * Time.deltaTime;
+			scaleY = Mathf.Min(scaleY, defaultScale.y);
+
+			transform.localScale = new Vector3(transform.localScale.x, scaleY, transform.localScale.z);
+		}
+	}
+}
