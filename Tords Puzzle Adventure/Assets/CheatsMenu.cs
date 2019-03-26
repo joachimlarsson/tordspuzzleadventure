31623cc9daf0673ae5e2795a5beaf2876afb40e9
Joachim Larsson <joachimlarsson90@gmail.com>
2019-03-26 18:03:40 +0100

    Add cheats menu to be used during dev
    * Add cheat to teleport character to start position

---
+++ Tords Puzzle Adventure/Assets/CheatsMenu.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CheatsMenu : MonoBehaviour
+{
+	public GameObject character;
+	public bool isVisible = true;
+
+	Vector3 characterStartPosition;
+
+	// GUI
+	float menuPosX = 20.0f;
+	float menuPosY = 80.0f;
+	float menuWidth = 240.0f;
+	float menuHeight = 240.0f;
+
+	float buttonPosX = 40.0f;
+	float buttonDefaultPosY = 120.0f;
+	float buttonPosY;
+	float buttonWidth = 200.0f;
+	float buttonHeight = 20.0f;
+	float buttonSpacingY = 10.0f;
+
+	bool createButton(string description)
+	{
+		bool isPressed = GUI.Button(new Rect(buttonPosX, buttonPosY, buttonWidth, buttonHeight), description);
+		buttonPosY += buttonHeight + buttonSpacingY;
+		
+		return isPressed;
+	}
+
+	void OnGUI()
+	{
+		if (!isVisible)
+			return;
+
+		buttonPosY = buttonDefaultPosY;
+
+		GUI.Box(new Rect(menuPosX, menuPosY, menuWidth, menuHeight), "Cheats");
+		if (createButton("Teleport character"))
+		{
+			character.transform.position = characterStartPosition;
+		}
+		if (createButton("TODO"))
+		{
+		}
+		if (createButton("TODO"))
+		{
+		}
+		if (createButton("TODO"))
+		{
+		}
+		if (createButton("TODO"))
+		{
+		}
+		if (createButton("Close menu"))
+		{
+			isVisible = false;
+		}
+	}
+
+	void Awake()
+	{
+		characterStartPosition = character.transform.position;
+	}
+}
