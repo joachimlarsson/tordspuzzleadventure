752a9e2c858f8136e36d2963c196241a6d581b6b
Joachim Larsson <joachimlarsson90@gmail.com>
2019-04-02 18:00:28 +0200

    Add start elevator to start platform

---
+++ Tords Puzzle Adventure/Assets/Scripts/StartElevator/StartElevatorMovementStart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class StartElevatorMovementStart : MonoBehaviour
+{
+	public float endPositionY = 0.0f;
+
+	void Update()
+	{
+		if (transform.position.y < endPositionY)
+		{
+			transform.position += new Vector3(0, 0.4f * Time.deltaTime, 0);
+		}
+	}
+}
