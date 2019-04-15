8910bf47b63c3cc22cda36978db84983fca8252a
Joachim Larsson <joachimlarsson90@gmail.com>
2019-04-15 19:16:54 +0200

    Add breaking mirror
    * On collision between character and mirror the reflection will be disabled
    * The glas splinters will ignore collision with the character

---
+++ Tords Puzzle Adventure/Assets/BreakingMirror.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakingMirror : MonoBehaviour
+{
+	public GameObject mirrorReflection;
+
+	void OnTriggerEnter()
+	{
+		gameObject.GetComponent<BreakableWindowIgnoreCharacter>().breakWindow();
+		mirrorReflection.SetActive(false);	
+	}
+}
