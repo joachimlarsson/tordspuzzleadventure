7b8149069478ed7981d9d4e0a20ca400b4285f67
Joachim Larsson <joachimlarsson90@gmail.com>
2019-04-23 16:54:55 +0200

    Add lobby finish script to start tutorial level

---
+++ Tords Puzzle Adventure/Assets/LobbyFinished.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyFinished : MonoBehaviour
+{
+	public float finishedPos = -4.5f;
+	bool runOnce = false;
+
+    void Update()
+    {
+        if (!runOnce && transform.position.y < finishedPos)
+		{
+			GameManager.instance.LoadTutorial();
+			runOnce = true;
+		}
+    }
+}
