f4dd98ab748123c6c2070ae9c6a538e4d7c341a2
Joachim Larsson <joachimlarsson90@gmail.com>
2019-06-10 17:34:07 +0200

    Add rotating cylinder

---
+++ Tords Puzzle Adventure/Assets/RotatingArm.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingArm : MonoBehaviour
+{
+	public GameObject rotatingArm;
+	public GameObject playerObject;
+
+	public float Angle = 0.15f;
+
+    void Update()
+    {
+		rotatingArm.transform.Rotate(0.0f, Angle, 0.0f, Space.Self);
+	}
+	
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.transform.SetParent(rotatingArm.transform, true);
+		}
+	}
+
+	void OnCollisionExit(Collision other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			other.transform.parent = null;
+		}
+	}
+}
